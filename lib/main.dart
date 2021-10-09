160ac6ab155677876edea7e85b5338380b65689b
Asha Choudhary <ashajakhar201@gmail.com>
2021-10-09 13:12:49 +0530

    themes extracted

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
 import 'package:flutter_catalog/util/routes.dart';
+import 'package:flutter_catalog/widgets/themes.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'pages/home_page.dart';
 
@@ -13,13 +14,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-          primarySwatch: Colors.deepPurple,
-          fontFamily: GoogleFonts.lato().fontFamily),
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darktTheme(context),
       debugShowCheckedModeBanner: false,
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-      ),
+      
       initialRoute: MyRoutes.homeRoute,
 
       routes: {
--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -8,21 +8,54 @@ class MyDrawer extends StatelessWidget {
       child: Container(
         color: Colors.deepPurple,
         child: ListView(
-         padding: EdgeInsets.zero,
+          padding: EdgeInsets.zero,
           children: [
             DrawerHeader(
               padding: EdgeInsets.zero,
               child: UserAccountsDrawerHeader(
-                margin: EdgeInsets.zero,
-                accountName: Text("asha"),
-                accountEmail: Text("ashajakhar201@gmail.com"),
-                currentAccountPicture: CircleAvatar(
-                  backgroundImage: NetworkImage(imageurl),
-                )
-                  
-                
-              ),
-            )
+                  margin: EdgeInsets.zero,
+                  accountName: Text("asha"),
+                  accountEmail: Text("ashajakhar201@gmail.com"),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundImage: NetworkImage(imageurl),
+                  )),
+            ),
+            ListTile(
+                leading: Icon(
+                  Icons.home,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  "Home",
+                  textScaleFactor: 1.2,
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                )),
+            ListTile(
+                leading: Icon(
+                  Icons.info_rounded,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  "Information",
+                  textScaleFactor: 1.2,
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                )),
+                ListTile(
+                leading: Icon(
+                  Icons.mail,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  "email_me",
+                  textScaleFactor: 1.2,
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ))
           ],
         ),
       ),
